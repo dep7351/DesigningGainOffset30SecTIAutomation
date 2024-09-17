150f184ac026c52b34f4b629b7804effe25fb606
dep7351 <Ddanny716@gmail.com>
2024-09-16 22:10:31 -0400

    added bones of the program

---
+++ DesigningGainOffsetProgram.py
@@ -0,0 +1,19 @@
+"""
+This is a program to take in user input and automate calculation of
+Gain and Offset for Op Amps per the Texas Instrument manual.
+Author: Daniel E Pittman Jr.
+Name: DesigningGainOffsetProgram.py
+Date: 9/16/2024
+"""
+from TIDesignManual import *
+
+
+
+def main():
+    print("hello world!!")
+
+    return 0  # program completed fine
+
+
+if __name__ == '__main__':
+    main()
---
+++ TIDesignManual.py
@@ -0,0 +1,39 @@
+"""
+This is a helper file that has functions that automate the steps
+for designing Op Amp circuitry from Texas Instruments' manual
+Name: DesigningGainOffsetProgram.py
+Date: 9/16/2024
+"""
+
+
+"""
+This is for section 3 of the manual which takes a
+positive m and a positive b
+"""
+def section3(m, b):
+
+    return 0
+
+"""
+This is for section 4 of the manual which takes a
+positive m and a negative b
+"""
+def section4(m, b):
+
+    return 0
+
+"""
+This is for section 5 of the manual which takes a
+negative m and a positive b
+"""
+def section5(m, b):
+
+    return 0
+
+"""
+This is for section 3 of the manual which takes a
+negative m and a negative b
+"""
+def section6(m, b):
+
+    return 0
